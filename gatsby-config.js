c50d49678d0c142deda6e9a7dc0f36977aa65035
Michael Washburn <mwashburn@hubspot.com>
2021-07-01 20:19:52 -0400

    checin progress

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Samantha Moyer | UX Writer",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
